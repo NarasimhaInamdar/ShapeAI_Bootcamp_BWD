440d964c0f9e86f14cbd005d88b2ea132fb08dd4
NarasimhaInamdar <85392300+NarasimhaInamdar@users.noreply.github.com>
2021-06-06 10:17:56 -0400

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React  from "react";
+
+function Note() 
+{
+  return (
+   <div className="note">
+     <h1>Java Script and React.JS</h1>
+     <p>
+       This was an amazing Bootcamp held by Shaurya Sinha 
+        where we were able to learn about JS and React.JS from 
+        the scratch and also some of the basic concepts of HTML.
+    </p>
+   </div>
+  );
+}
+
+export default Note;
---
+++ Note1.jsx
@@ -0,0 +1,18 @@
+import React  from "react";
+
+function Note1() 
+{
+  return (
+   <div className="note">
+     <h1>Python and Machine Learnig</h1>
+     <p>
+       This was an amazing Bootcamp held by Shaurya Sinha 
+        where we were able to learn about Python from 
+        the scratch and also we were taught the basic 
+        topics of Machine Learning.
+    </p>
+   </div>
+  );
+}
+
+export default Note1;
